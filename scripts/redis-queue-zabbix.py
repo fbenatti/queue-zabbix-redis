8b64721407d71bbac3f3a8f69f0bfe0223fc24de
Fernando H. Benatti <fhenriquebenatti@gmail.com>
2020-02-06 10:51:38 -0300

    Update redis-queue-zabbix.py
    Fix password

--- scripts/redis-queue-zabbix.py
+++ scripts/redis-queue-zabbix.py
@@ -8,8 +8,9 @@ import sys
 hostname = socket.gethostname()
 redishost=hostname
 redisport="6379"
+password=''
 
-r = redis.Redis(host=redishost,port=redisport)
+r = redis.Redis(host=redishost,port=redisport,password=password)
 listkeys=r.keys('*')
 
 ##Return discovery
@@ -17,10 +18,10 @@ print '{"data":['
 fila = []
 n=0
 nome_fila = []
-for key in listkeys:  
-	print('{"{#FILA}":"%s"},' % (key))
+for key in listkeys:
+        print('{"{#FILA}":"%s"},' % (key))
 
-print('{"{#FILA}":"TotalObjetos"}')	
+print('{"{#FILA}":"TotalObjetos"}')
 print(']}')
 
 ##Update items.
@@ -28,18 +29,18 @@ data1 = {}
 data2 = {}
 totobjs=0
 
-for key in listkeys:  
+for key in listkeys:
+  if r.type(key) == "list":
     totitens=(r.llen(key))
-    kb = "fila.redis[" + key + "]"
     data2[key] = totitens
     totobjs = totobjs + totitens
 
+
 kb = 'fila.redis[TotalObjetos]'
-data2[kb] = totobjs	
+data2[kb] = totobjs
 data1[hostname] = data2
 DATA = json.loads(json.dumps(data1))
 zbx = protobix.DataContainer()
 zbx.data_type = 'items'
 zbx.add(DATA)
 zbx.send()
-
